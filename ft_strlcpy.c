9f2f6f300408f3fc80fb190b3d2f22295bce4c51
Zakariya Dadsi <zdadsi@e1r3p5.1337.ma>
2025-10-19 05:21:00 +0100

    add `ft_strlcpy.c`

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,11 +6,12 @@
 /*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 01:22:42 by zdadsi            #+#    #+#             */
-/*   Updated: 2025/10/18 01:23:54 by zdadsi           ###   ########.fr       */
+/*   Updated: 2025/10/19 05:17:52 by zdadsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdio.h>
 
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize) {
   char *d = dst;
@@ -21,10 +22,20 @@ size_t ft_strlcpy(char *dst, const char *src, size_t dstsize) {
     while (--n && (*d++ = *s++) != '\0')
       ;
     *d = '\0';
+    // *(--d) = '\0';
+            printf("%ld", d - dst);
   }
 
   while (*s++)
     ;
 
   return --s - src;
+}
+
+int main() {
+    char dst[3] = "a";
+    char *src = "c";
+
+    ft_strlcpy(dst, src, 2);
+    printf("%s", dst);
 }
