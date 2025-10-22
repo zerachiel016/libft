f493bb8217a701380f86f9bc8a45a079570221bb
zerachiel016 <z.dadsi21@gmail.com>
2025-10-22 23:50:49 +0100

    fix(ft_strncmp): not handling the `size_t n = 0` case

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,15 +6,15 @@
 /*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 21:12:28 by zdadsi            #+#    #+#             */
-/*   Updated: 2025/10/22 21:24:59 by zdadsi           ###   ########.fr       */
+/*   Updated: 2025/10/22 23:50:34 by zdadsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
 int ft_strncmp(const char *s1, const char *s2, register size_t n){
-
-    while(n-- && *s1++ == *s2++)
-        ;
-    return (unsigned char )*--s1 - (unsigned char)*--s2;
+    while(n--)
+        if(*s1++ != *s2++)
+            return *(unsigned char *)--s1 - *(unsigned char *)--s2;
+    return 0;
 }
