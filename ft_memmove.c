0ac74593de4036c8abfff9742f23ed19de10bb6d
zerachiel016 <z.dadsi21@gmail.com>
2025-10-22 18:26:37 +0100

    style(ft_memmove): norminette!

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,26 +6,25 @@
 /*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 22:55:35 by zdadsi            #+#    #+#             */
-/*   Updated: 2025/10/16 23:33:24 by zdadsi           ###   ########.fr       */
+/*   Updated: 2025/10/22 18:12:08 by zdadsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void *ft_memmove(void *dest, const void *src, size_t n){
-    unsigned char *d = dest;
-    const unsigned char *s = src;
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s = src;
 
-    if(d == s)
-        return dest;
-    else if ( s < d){
-        while(n--)
-            *(d+n) = *(s+n);
-    }
-    else {
-        while(n--)
-            *d++ = *s++;
-    }
-
-    return dest;
+	d = dest;
+	if (d == s)
+		return (dest);
+	else if (s < d)
+		while (n--)
+			*(d + n) = *(s + n);
+	else
+		while (n--)
+			*d++ = *s++;
+	return (dest);
 }
