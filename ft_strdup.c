aab53c1d727434b8fa9c2f95a55e56c505142704
zerachiel016 <z.dadsi21@gmail.com>
2025-10-23 06:11:59 +0100

    feat: implement strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/14 22:53:13 by zdadsi            #+#    #+#             */
-/*   Updated: 2025/10/23 06:04:52 by zdadsi           ###   ########.fr       */
+/*   Created: 2025/10/23 05:58:13 by zdadsi            #+#    #+#             */
+/*   Updated: 2025/10/23 06:11:37 by zdadsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#include "libft.h"
+#include <stdlib.h>
 
-#include <stddef.h>
+char *ft_strdup(const char *s) {
+    size_t len = ft_strlen(s) + 1;
+    void *new = malloc(len);
 
-int ft_isdigit(int c);
-int ft_isalpha(int c);
-void *ft_memset(void *s, int c, size_t n);
-size_t ft_strlen(const char *s);
-int ft_strncmp(const char *s1, const char *s2, size_t n);
-void ft_bzero(void *s, size_t n);
-void *ft_memcpy(void *dest, const void *src, size_t n);
+    if(!new)
+        return new;
+
+    return ft_memcpy(new, s, len);
+}
