3fb8dc03ec086a832ac7a3c33677a6c26859a3c7
zerachiel016 <z.dadsi21@gmail.com>
2025-10-24 20:18:50 +0100

    feat: implement ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/24 19:58:28 by zdadsi            #+#    #+#             */
+/*   Updated: 2025/10/24 20:18:41 by zdadsi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*new;
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new = malloc(s1_len + s2_len + 1);
+	if (new)
+		ft_memcpy(ft_memcpy(new, s1, s1_len) + s1_len, s2, s2_len);
+	return (new);
+}
