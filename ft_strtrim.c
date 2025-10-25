d36eece9888a2cf5366a808d82db7e37217db6a3
zerachiel016 <z.dadsi21@gmail.com>
2025-10-25 06:33:44 +0100

    feat: implement ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/24 20:19:42 by zdadsi            #+#    #+#             */
+/*   Updated: 2025/10/25 02:07:55 by zdadsi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char *ft_strtrim(char const *s1, char const *set) {
+
+  while (ft_strchr(set, *s1))
+    s1++;
+
+  size_t r_trim = ft_strlen(s1);
+
+  while (r_trim && ft_strchr(set, s1[r_trim - 1]))
+    r_trim--;
+
+  char *trimmed = malloc(r_trim + 1);
+  if (trimmed)
+    trimmed = ft_memcpy(trimmed, s1, r_trim);
+
+  return trimmed;
+}
+
+//  NOTE: Old Version
+// char *ft_strtrim(char const *s1, char const *set) {
+//
+//   size_t len = ft_strlen(s1);
+//   size_t l_trim = 0;
+//   size_t r_trim = 0;
+//
+//   while (s1[l_trim] && ft_strchr(set, s1[l_trim]))
+//     l_trim++;
+//
+//   if (l_trim != len)
+//     while (s1[len - r_trim - 1] && ft_strchr(set, s1[len - r_trim - 1]))
+//       r_trim++;
+//
+//   len -= l_trim + r_trim;
+//   char *trimmed = malloc(len);
+//
+//   if (trimmed)
+//     trimmed = ft_memcpy(trimmed, s1 + l_trim, len);
+//
+//   return trimmed;
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:53:13 by zdadsi            #+#    #+#             */
-/*   Updated: 2025/10/23 06:04:52 by zdadsi           ###   ########.fr       */
+/*   Updated: 2025/10/25 01:20:26 by zdadsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,4 @@ size_t ft_strlen(const char *s);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 void ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
+char *ft_strchr(const char *s, int c);
