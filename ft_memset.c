3c1199f4bcb13b1f3054c6aa0c615df980fa8e05
zerachiel016 <z.dadsi21@gmail.com>
2025-10-25 21:02:36 +0100

    chore(ft_memset): remove unneeded includes

--- ft_memset.c
+++ ft_memset.c
@@ -6,12 +6,11 @@
 /*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 17:45:44 by zdadsi            #+#    #+#             */
-/*   Updated: 2025/10/16 15:12:34 by zdadsi           ###   ########.fr       */
+/*   Updated: 2025/10/25 21:01:24 by zdadsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include <stdint.h>
 
 // NOTE: Could be more optimized like the original implementations
 void *ft_memset(void *s, int c, size_t n) {
