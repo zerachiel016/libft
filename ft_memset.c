9a3923f108f226b1d4cd08dea0735cc9e8b48892
zerachiel016 <z.dadsi21@gmail.com>
2025-10-27 06:14:07 +0100

    chore(ft_memset): pin-point what kind of optimisation I meant (so I don't forget)

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 17:45:44 by zdadsi            #+#    #+#             */
-/*   Updated: 2025/10/25 21:01:24 by zdadsi           ###   ########.fr       */
+/*   Updated: 2025/10/27 05:18:00 by zdadsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-// NOTE: Could be more optimized like the original implementations
+// PERF: Could be more optimized with (64-bit bulk writes, SIMD operations etc...)
 void *ft_memset(void *s, int c, size_t n) {
   unsigned char *p = (unsigned char *)s;
   unsigned char cc = c & 0xff;
