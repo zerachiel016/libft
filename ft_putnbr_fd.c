932830e8dc9798b097c7a2bee014fbee05f69bc9
zerachiel016 <z.dadsi21@gmail.com>
2025-10-27 12:38:19 +0100

    feat: implement ft_putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,45 @@
+#include <limits.h>
+#include <unistd.h>
+
+static int count_digits(unsigned int n) {
+  if (n < 10)
+    return 1;
+  if (n < 100)
+    return 2;
+  if (n < 1000)
+    return 3;
+  if (n < 10000)
+    return 4;
+  if (n < 100000)
+    return 5;
+  if (n < 1000000)
+    return 6;
+  if (n < 10000000)
+    return 7;
+  if (n < 100000000)
+    return 8;
+  if (n < 1000000000)
+    return 9;
+  return 10;
+}
+
+static void mini_itoa(int n, char *s, int *len) {
+  int neg = (n < 0);
+  unsigned int _n = (n ^ -neg) + neg;
+  int digits = neg + count_digits(_n);
+  *len = digits;
+
+  while (digits--) {
+    s[digits] = (_n % 10) + '0';
+    _n /= 10;
+  }
+  if (neg)
+    *s = '-';
+}
+
+void ft_putnbr_fd(int n, int fd) {
+  int len;
+  char s[11];
+  mini_itoa(n, s, &len);
+  write(fd, s, len);
+}
