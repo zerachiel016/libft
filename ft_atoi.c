b058896b981f2f450b9e6294be761843bfe50ef9
zerachiel016 <z.dadsi21@gmail.com>
2025-10-28 07:44:39 +0100

    refactor(ft_atoi): forgot and swapped var names

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: zdadsi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 00:28:48 by zdadsi            #+#    #+#             */
-/*   Updated: 2025/10/27 11:28:30 by zdadsi           ###   ########.fr       */
+/*   Updated: 2025/10/28 07:44:12 by zdadsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 int	ft_atoi(const char *nbr)
 {
 	unsigned long	res;
-	unsigned long	lim;
-	int				offset;
+	unsigned long	offset;
+	int				lim;
 	int				neg;
 
 	res = 0;
@@ -24,12 +24,12 @@ int	ft_atoi(const char *nbr)
 		nbr++;
 	neg = (*nbr == '-');
 	nbr += (*nbr == '-' || *nbr == '+');
-	lim = LONG_MAX + neg;
-	offset = 7 + neg;
-	lim /= 10;
+	offset = LONG_MAX + neg;
+	lim = 7 + neg;
+	offset /= 10;
 	while ('0' <= *nbr && *nbr <= '9')
 	{
-		if (res > lim || (res == lim && (*nbr - '0') > offset))
+		if (res > offset || (res == offset && (*nbr - '0') > lim))
 			return (-(!neg));
 		res = res * 10 + (*nbr++ - '0');
 	}
