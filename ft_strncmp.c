1a6b594390292eb96d02ddcbcf1aa9ffe25b6f45
zerachiel016 <z.dadsi21@gmail.com>
2025-11-05 18:52:33 +0100

    fix(ft_strncmp): return 0 when both `s1` & `s2` are NULL

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,8 +14,9 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n--)
-		if (*s1++ != *s2++)
-			return (*(unsigned char *)--s1 - *(unsigned char *)--s2);
+	if (s1 != s2)
+		while (n--)
+			if (*s1++ != *s2++)
+				return (*(unsigned char *)--s1 - *(unsigned char *)--s2);
 	return (0);
 }
