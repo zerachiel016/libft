d4b4e7ac73445f3f9a6fcaee88bcdfe3cc5a237d
zerachiel016 <z.dadsi21@gmail.com>
2025-11-05 19:00:26 +0100

    fix(ft_memcmp): return 0 when both `s1` and `s2` are NULL

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -17,8 +17,9 @@ int	ft_memcmp(const void *str1, const void *str2, size_t n)
 	const unsigned char	*s1 = str1;
 	const unsigned char	*s2 = str2;
 
-	while (n--)
-		if (*s1++ != *s2++)
-			return (*--s1 - *--s2);
+    if(s1 != s2)
+        while (n--)
+            if (*s1++ != *s2++)
+                return (*--s1 - *--s2);
 	return (0);
 }
