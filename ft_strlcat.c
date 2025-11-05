7b4fa64ff78bb27774745d3ad0a727633fdb0b9e
zerachiel016 <z.dadsi21@gmail.com>
2025-11-05 18:03:02 +0100

    fix(ft_strlcat): return `src` len when `dst == NULL`

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -21,8 +21,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 	d = dst;
 	n = size;
-	while (*d && n--)
-		d++;
+	if (d)
+		while (*d && n--)
+			d++;
 	dlen = d - dst;
 	if (n)
 	{
