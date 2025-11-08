38826064cf56be38906b78f3fb066f7f08fd2354
zerachiel016 <z.dadsi21@gmail.com>
2025-11-08 05:33:10 +0100

    fix(ft_strncmp): missing null-character checking

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -15,7 +15,7 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (s1 != s2)
-		while (n--)
+		while (n-- && *s1 && *s2)
 			if (*s1++ != *s2++)
 				return (*(unsigned char *)--s1 - *(unsigned char *)--s2);
 	return (0);
