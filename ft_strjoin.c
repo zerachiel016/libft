0d81d575447b600ae1bf467bc0bff29ccdc8b71e
zerachiel016 <z.dadsi21@gmail.com>
2025-11-09 02:55:17 +0100

    fix(ft_strjoin): add missing null-character

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -29,6 +29,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s2_len = ft_strlen(s2);
 	new = malloc(s1_len + s2_len + 1);
 	if (new)
-		ft_memcpy(ft_memcpy(new, s1, s1_len) + s1_len, s2, s2_len);
+		ft_memcpy(ft_memcpy(new, s1, s1_len) + s1_len, s2, s2_len + 1);
 	return (new);
 }
