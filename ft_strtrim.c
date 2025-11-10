07d923cf4f5dd7d80335d7dadae35064ef9d86d4
zerachiel016 <z.dadsi21@gmail.com>
2025-11-10 04:56:18 +0100

    chore(ft_strtrim): remove redundant comment

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -25,27 +25,3 @@ char	*ft_strtrim(char const *s1, char const *set)
 		end--;
 	return (ft_substr(s1, 0, end));
 }
-
-//  NOTE: Old ft_strtrim Version
-//
-// char *ft_strtrim(char const *s1, char const *set) {
-//
-//   size_t len = ft_strlen(s1);
-//   size_t l_trim = 0;
-//   size_t r_trim = 0;
-//
-//   while (s1[l_trim] && ft_strchr(set, s1[l_trim]))
-//     l_trim++;
-//
-//   if (l_trim != len)
-//     while (s1[len - r_trim - 1] && ft_strchr(set, s1[len - r_trim - 1]))
-//       r_trim++;
-//
-//   len -= l_trim + r_trim;
-//   char *trimmed = malloc(len);
-//
-//   if (trimmed)
-//     trimmed = ft_memcpy(trimmed, s1 + l_trim, len);
-//
-//   return (trimmed);
-// }
