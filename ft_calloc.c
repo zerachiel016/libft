93d3396df0933419c522916a10b7307445650fa1
zerachiel016 <z.dadsi21@gmail.com>
2025-11-10 04:48:57 +0100

    fix(ft_calloc): add a missing size_t overflow check

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,12 @@
 /*   By: zdadsi <zdadsi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 18:24:45 by zdadsi            #+#    #+#             */
-/*   Updated: 2025/11/03 18:24:45 by zdadsi           ###   ########.fr       */
+/*   Updated: 2025/11/10 03:11:32 by zdadsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdint.h>
 #include <stdlib.h>
 
 void	*ft_calloc(size_t n, size_t size)
@@ -19,6 +20,8 @@ void	*ft_calloc(size_t n, size_t size)
 
 	if (n == 0 || size == 0)
 		return (malloc(0));
+	if (n > SIZE_MAX / size)
+		return (NULL);
 	p = malloc(n * size);
 	if (p)
 		ft_bzero(p, n * size);
